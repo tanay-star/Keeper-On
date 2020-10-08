ed5bf034a5c63e17d667a9d44bc66eccda172d98
Tanay Banerjee <banerjeetanay333@gmail.com>
2020-10-08 18:45:25 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Switch, Route } from 'react-router-dom'
+//importing componnets
+import HomePage from './pages/homepage/homepage'
+import Header from './components/header/header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
